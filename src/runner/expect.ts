494ea10f024f802805231f229eea3ec212936ec2
Nicolas Jacquemin <nicolas1.jacquemin@epitech.eu>
2023-03-20 22:11:39 +0100

    [+] Init Tests runner

---
+++ src/runner/expect.ts
@@ -0,0 +1,7 @@
+import {Runner, Test, Out} from '../modules/types.js';
+
+function runExpect(runner: Runner, test: Test): boolean {
+    return true;
+}
+
+export default runExpect;
---
+++ src/runner/refer.ts
@@ -0,0 +1,7 @@
+import {Runner, Test, Out} from '../modules/types.js';
+
+function runRefer(runner: Runner, test: Test): boolean {
+    return true;
+}
+
+export default runRefer;
